9901d6c1150ad1859ec5553789c478b2beecc301
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-13 15:31:44 +0530

    landRoute saved

--- server/Routes/landRoute.js
+++ server/Routes/landRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createLand, getAllLands, getLand } from "../controllers/landCntrl.js";
+
 const router = express.Router();
 
 router.post("/create", createLand);
