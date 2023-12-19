e87acbca4992b8ebfb9f482e5673e83dad77cb9b
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-19 12:19:52 +0530

    change commit

--- client/src/components/NewProjectCard/NewProjectCard.jsx
+++ client/src/components/NewProjectCard/NewProjectCard.jsx
@@ -9,7 +9,7 @@ const NewProjectCard = ({ card }) => {
       className="np-card"
       onClick={() => navigate(`../residencies/${card.id}`)}
     >
-      <span className="flexCenter">
+      <span className="flexCenter imggg">
         <img src={card.image} alt="home" />
       </span>
 
