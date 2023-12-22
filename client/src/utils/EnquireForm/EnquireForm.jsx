49625c323c3ab38c30585c7be1e51e5a82255d88
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-22 18:27:42 +0530

    usestate commit

--- client/src/utils/EnquireForm/EnquireForm.jsx
+++ client/src/utils/EnquireForm/EnquireForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./EnquireForm.css";
 import emailjs from "@emailjs/browser";
 import { FaCheckCircle } from "react-icons/fa";
