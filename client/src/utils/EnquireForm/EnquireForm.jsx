c255a04db909e2c8347e6d0c960608e3e749c463
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-24 19:40:34 +0530

    commit changes

--- client/src/utils/EnquireForm/EnquireForm.jsx
+++ client/src/utils/EnquireForm/EnquireForm.jsx
@@ -72,7 +72,7 @@ const EnquireForm = () => {
               <div className="popup show">
                 <FaCheckCircle size={100} className="thankicon" />
                 <h2>Thank You for Contacting VSV!</h2>
-                <button type="button" onClick="closePopup">
+                <button type="submit" onClick="closePopup">
                   OK
                 </button>
               </div>
