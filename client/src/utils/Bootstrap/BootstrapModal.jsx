7f953e0d4704bb251184721f8c8f7bb3a036fbb0
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-25 23:17:49 +0530

    commit

--- client/src/utils/Bootstrap/BootstrapModal.jsx
+++ client/src/utils/Bootstrap/BootstrapModal.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function BootstrapModal() {
   const [show, setShow] = useState(false);
