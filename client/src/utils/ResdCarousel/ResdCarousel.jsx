7f3d966646b70b537b6a2a45eb4775547e44d411
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-29 22:51:26 +0530

    commit

--- client/src/utils/ResdCarousel/ResdCarousel.jsx
+++ client/src/utils/ResdCarousel/ResdCarousel.jsx
@@ -13,69 +13,37 @@ const ResdCarousel = () => {
     <div className="carousel-container">
       <Carousel data-bs-theme="dark">
         <Carousel.Item>
-          <img
-            style={{ height: "70vh" }}
-            className="d-block w-100"
-            src={data?.image1}
-            alt="First slide"
-          />
+          <img className="d-block w-100" src={data?.image1} alt="First slide" />
           <Carousel.Caption>
             <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
-            style={{ height: "70vh" }}
             className="d-block w-100"
             src={data?.image2}
             alt="Second slide"
           />
           <Carousel.Caption>
             <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            style={{ height: "70vh" }}
-            className="d-block w-100"
-            src={data?.image3}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={data?.image3} alt="Third slide" />
           <Carousel.Caption>
             <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            style={{ height: "70vh" }}
-            className="d-block w-100"
-            src={data?.image4}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={data?.image4} alt="Third slide" />
           <Carousel.Caption>
             <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            style={{ height: "70vh" }}
-            className="d-block w-100"
-            src={data?.image5}
-            alt="Third slide"
-          />
+          <img className="d-block w-100" src={data?.image5} alt="Third slide" />
           <Carousel.Caption>
             <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
