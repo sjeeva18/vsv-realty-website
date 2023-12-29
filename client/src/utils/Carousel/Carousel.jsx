1bd8b3e3616f07bcce7fca1660cff0179c91f38f
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-29 16:45:23 +0530

    this too

--- client/src/utils/Carousel/Carousel.jsx
+++ client/src/utils/Carousel/Carousel.jsx
@@ -6,39 +6,26 @@ import { useLocation } from "react-router-dom";
 const CarouselPage = () => {
   const { pathname } = useLocation();
   const id = pathname.split("/").slice(-1)[0];
-  const { data } = useQuery(["resd", id], () =>
-    getResidency(id)
-  );
+  const { data } = useQuery(["resd", id], () => getResidency(id));
+  console.log(data);
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={data?.image1}
-          alt="First slide"
-        />
+        <img className="d-block w-100" src={data?.image1} alt="First slide" />
         <Carousel.Caption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={data?.image2}
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src={data?.image2} alt="Second slide" />
         <Carousel.Caption>
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={data?.image3}
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src={data?.image3} alt="Third slide" />
         <Carousel.Caption>
           <h5>Third slide label</h5>
           <p>
