69297a4d55a6138335a5bc824b7f51bfe7fb0d18
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-29 21:50:31 +0530

    commit changes

--- client/src/utils/Carousel/Carousel.jsx
+++ client/src/utils/Carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 import { getResidency } from "../api";
-import "./Carousel.css"
+import "./Carousel.css";
 
 const CarouselPage = () => {
   const { pathname } = useLocation();
@@ -11,38 +11,23 @@ const CarouselPage = () => {
   const { data } = useQuery(["resd", id], () => getResidency(id));
   console.log(data);
   return (
-    <Carousel data-bs-theme="dark" className="carousel-container">
+    <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <img
-          style={{ height: "80vh" }}
-          className="d-block w-100"
-          src={data?.image1}
-          alt="First slide"
-        />
+        <img className="d-block w-100" src={data?.image1} alt="First slide" />
         <Carousel.Caption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          style={{ height: "80vh" }}
-          className="d-block w-100"
-          src={data?.image2}
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src={data?.image2} alt="Second slide" />
         <Carousel.Caption>
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          style={{ height: "80vh" }}
-          className="d-block w-100"
-          src={data?.image3}
-          alt="Third slide"
-        />
+        <img className="d-block w-100" src={data?.image3} alt="Third slide" />
         <Carousel.Caption>
           <h5>Third slide label</h5>
           <p>
