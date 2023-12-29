7ac0b5755f41541760e517cde7b7c29aadbaf711
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-29 17:03:01 +0530

    commit

--- client/src/utils/Carousel/Carousel.jsx
+++ client/src/utils/Carousel/Carousel.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 import { getResidency } from "../api";
-import "./Carousel.css"
+import "./Carousel.css";
 
 const CarouselPage = () => {
   const { pathname } = useLocation();
@@ -11,10 +11,10 @@ const CarouselPage = () => {
   const { data } = useQuery(["resd", id], () => getResidency(id));
   console.log(data);
   return (
-    <Carousel data-bs-theme="dark" className="flexCenter innerWidth Paddings">
+    <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "80vh" }}
           className="d-block w-100"
           src={data?.image1}
           alt="First slide"
@@ -26,7 +26,7 @@ const CarouselPage = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "80vh" }}
           className="d-block w-100"
           src={data?.image2}
           alt="Second slide"
@@ -38,7 +38,7 @@ const CarouselPage = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "80vh" }}
           className="d-block w-100"
           src={data?.image3}
           alt="Third slide"
