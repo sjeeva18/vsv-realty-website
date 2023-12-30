504ece53534a473ee3ec79020784e42f8e5ec77e
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-30 12:35:18 +0530

    commit

--- client/src/utils/ResdCarousel/ResdCarousel.jsx
+++ client/src/utils/ResdCarousel/ResdCarousel.jsx
@@ -13,7 +13,7 @@ const ResdCarousel = () => {
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "90vh" }}
           className="d-block w-100"
           src={data?.image1}
           alt="First slide"
@@ -24,7 +24,7 @@ const ResdCarousel = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "90vh" }}
           className="d-block w-100"
           src={data?.image2}
           alt="Second slide"
@@ -35,7 +35,7 @@ const ResdCarousel = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "90vh" }}
           className="d-block w-100"
           src={data?.image3}
           alt="Third slide"
@@ -46,7 +46,7 @@ const ResdCarousel = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "90vh" }}
           className="d-block w-100"
           src={data?.image4}
           alt="Third slide"
@@ -57,7 +57,7 @@ const ResdCarousel = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          style={{ height: "70vh" }}
+          style={{ height: "90vh" }}
           className="d-block w-100"
           src={data?.image5}
           alt="Third slide"
