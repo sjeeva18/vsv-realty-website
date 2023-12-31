35e101b0f297a47a57807eb1293a6e4b8037ecb5
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-31 13:38:44 +0530

    commit

--- client/src/utils/ResdCarousel/ResdCarousel.jsx
+++ client/src/utils/ResdCarousel/ResdCarousel.jsx
@@ -12,49 +12,52 @@ const ResdCarousel = () => {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
-        <div className="Carousel-container">
-          <img className="d-block w-100" src={data?.image1} alt="First slide" />
-        </div>
+        <img
+          className="d-block w-100 Carousel-container"
+          src={data?.image1}
+          alt="First slide"
+        />
+
         <Carousel.Caption>
           <h5>First slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="Carousel-container">
-          <img
-            className="d-block w-100"
-            src={data?.image2}
-            alt="Second slide"
-          />
-        </div>
+        <img
+          className="d-block w-100 Carousel-container"
+          src={data?.image2}
+          alt="Second slide"
+        />
         <Carousel.Caption>
           <h5>Second slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="Carousel-container">
-          <img className="d-block w-100" src={data?.image3} alt="Third slide" />
-        </div>
+        <img
+          className="d-block w-100 Carousel-container"
+          src={data?.image3}
+          alt="Third slide"
+        />
         <Carousel.Caption>
           <h5>Third slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="Carousel-container">
-          <img
-            className="d-block w-100 "
-            src={data?.image4}
-            alt="Third slide"
-          />
-        </div>
+        <img
+          className="d-block w-100 Carousel-container"
+          src={data?.image4}
+          alt="Third slide"
+        />
         <Carousel.Caption>
           <h5>Third slide label</h5>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="Carousel-container">
-          <img className="d-block w-100" src={data?.image5} alt="Third slide" />
-        </div>
+        <img
+          className="d-block w-100 Carousel-container"
+          src={data?.image5}
+          alt="Third slide"
+        />
         <Carousel.Caption>
           <h5>Third slide label</h5>
         </Carousel.Caption>
