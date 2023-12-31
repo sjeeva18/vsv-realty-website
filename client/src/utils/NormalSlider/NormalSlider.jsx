99a9ae55e6f774f6e01d40948905ac124111600f
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2023-12-31 19:17:44 +0530

    commit

--- client/src/utils/NormalSlider/NormalSlider.jsx
+++ client/src/utils/NormalSlider/NormalSlider.jsx
@@ -11,35 +11,33 @@ const NormalSlider = () => {
   const { data } = useQuery(["resd", id], () => getResidency(id));
   console.log(data);
   return (
-    <div className="paddings innerWidth flexCenter">
-      <Swiper>
-        <SwiperSlide>
-          <div className="normal-container">
-            <img src={data?.image1} alt="residency image" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="normal-container">
-            <img src={data?.image2} alt="residency image" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="normal-container">
-            <img src={data?.image3} alt="residency image" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="normal-container">
-            <img src={data?.image4} alt="residency image" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="normal-container">
-            <img src={data?.image5} alt="residency image" />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+    <Swiper>
+      <SwiperSlide>
+        <div className="normal-container">
+          <img src={data?.image1} alt="residency image" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="normal-container">
+          <img src={data?.image2} alt="residency image" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="normal-container">
+          <img src={data?.image3} alt="residency image" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="normal-container">
+          <img src={data?.image4} alt="residency image" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="normal-container">
+          <img src={data?.image5} alt="residency image" />
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 };
 
