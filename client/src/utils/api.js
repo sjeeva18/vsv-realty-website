5810ce5f1f7fa6a26fd639c9cd32b4f38ccb2280
sjeeva18 <113793826+sjeeva18@users.noreply.github.com>
2024-01-02 18:17:05 +0530

    commit

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://localhost:8000/api",
+  baseURL: "https://vsvrealty.vercel.app/api",
 });
 
 export const getAllResidencies = async () => {
